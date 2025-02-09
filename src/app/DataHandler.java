f317cf8ffaee45314fc911f175af98cb3f097e53
Appolo-13 <rayl1x.gta@gmail.com>
2025-02-09 23:19:49 +0100

    fix all errors

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -1,12 +1,14 @@
 package app;
 
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class DataHandler {
 
-    private final Locker lock = new
+    private final ReentrantLock lock = new ReentrantLock();
 
     public synchronized int modify(int num) {
-        lock.();
+        lock.lock();
         try {
             num = num * 3;
             return num;
